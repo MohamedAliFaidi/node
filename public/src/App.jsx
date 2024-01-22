958deb99c23c93c83a0349df8e5e0ed254c7f33a
MohamedAliFaidi <dalyyy3259@protonmail.com>
2024-01-22 13:03:50 +0100

    test

--- public/src/App.jsx
+++ public/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
     <div className="App">
       <button
       onClick={()=>{
-        fetch("/api").then((response)=>{
+        fetch("/v1/api").then((response)=>{
           return response.json()
         }).then((data)=>{
           console.log(data)
