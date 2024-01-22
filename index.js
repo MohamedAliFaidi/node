737afb9e7cceb090185644811906c9288b860b27
MohamedAliFaidi <dalyyy3259@protonmail.com>
2024-01-22 14:38:46 +0100

    test

--- index.js
+++ index.js
@@ -31,20 +31,13 @@ app.disable("x-powered-by");
 
 app.use(express.json());
 
-app.use((req, res, next) => {
-  res.header('Content-Type', 'application/javascript');
-  next();
-});
 
-if (process.env.NODE_ENV === 'production') {
-  // In production, serve the built React Vite app from the 'public/my-vite-app/dist' directory
-  app.use(express.static( path.join(__dirname, 'public', '/', 'dist')));
 
-  // Handle all other routes by serving the React app's entry point
+
  
   app.get('/api', (req, res) => {
     res.json({ status: 200, statusText:"OK"})
-  })}
+  })
 
 
 
