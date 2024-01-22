d38849e347fe1db71e91f8a93c71935eaa97ecf9
MohamedAliFaidi <dalyyy3259@protonmail.com>
2024-01-22 15:06:40 +0100

    test

--- index.js
+++ index.js
@@ -21,8 +21,20 @@ app.use(compression());
 
 app.use(
   cors({
-    origin: "*",
+    origin: "https://client-delta-gold.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+    allowedHeaders :[
+      "Origin",
+      "X-Requested-With",
+      "Content-Type",
+      "Accept",
+      "Authorization",
+      "Access-Control-Allow-Headers",
+      "Access-Control-Allow-Origin",
+      "Access-Control-Allow-Methods",
+      "Access-Control-Allow-Credentials"
+    ]
   })
 );
 
