2ab90d72d427393a8406fb449dbc98c619bb72e7
MohamedAliFaidi <dalyyy3259@protonmail.com>
2024-01-22 13:15:58 +0100

    test

--- public/vite.config.js
+++ public/vite.config.js
@@ -13,25 +13,7 @@ export default defineConfig({
       },
     },
   },
-  server: {
-    proxy: {
-      "/v0": {
-        target: "https://node-sage-six.vercel.app",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/v0/, ""), 
-      },
-      "/v1": {
-        target: "https://node-git-main-mohamedalifaidi.vercel.app",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/v1/, ""),
-      },
-      "/v2": {
-        target: "https://node-res2s48hy-mohamedalifaidi.vercel.app",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/v2/, ""),
-      },
-    },
-  },
+
   // optimizeDeps: {
   //   include: ["axios"], // Include axios in the optimized dependencies
   // },
@@ -42,7 +24,7 @@ export default defineConfig({
       return (req, res, next) => {
         res.setHeader(
           "Content-Security-Policy",
-          "default-src 'self'; connect-src 'self' 'https://node-sage-six.vercel.app' 'https://node-git-main-mohamedalifaidi.vercel.app' 'https://node-res2s48hy-mohamedalifaidi.vercel.app';"
+          "default-src 'self' 'https://node-sage-six.vercel.app' 'https://node-git-main-mohamedalifaidi.vercel.app' ; connect-src 'self' 'https://node-sage-six.vercel.app' 'https://node-git-main-mohamedalifaidi.vercel.app' 'https://node-res2s48hy-mohamedalifaidi.vercel.app';"
         );
         next();
       };
