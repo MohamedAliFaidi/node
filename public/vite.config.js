d8685d32da8eaeaf8046e41cf87b8903451a471a
MohamedAliFaidi <dalyyy3259@protonmail.com>
2024-01-22 12:34:13 +0100

    test

--- public/vite.config.js
+++ public/vite.config.js
@@ -1,8 +1,40 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base : '/'
-})
+  base: "/",
+  // vite.config.js
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined, // Allow Vite to manage chunks
+      },
+    },
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://node-sage-six.vercel.app",
+        changeOrigin: true,
+      },
+    },
+  },
+  optimizeDeps: {
+    include: ["axios"], // Include axios in the optimized dependencies
+  },
+  // Add Content Security Policy configuration
+  // Make sure to replace 'https://node-sage-six.vercel.app' with your actual API domain
+  serverMiddleware: {
+    configure: (app) => {
+      app.use((req, res, next) => {
+        res.setHeader(
+          "Content-Security-Policy",
+          "default-src 'self'; connect-src 'self' https://node-sage-six.vercel.app;"
+        );
+        next();
+      });
+    },
+  },
+});
